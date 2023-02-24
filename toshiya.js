3c079badc0d92e031b2a33f34f3cd2bdbd2eea6a
Toshiya <toshiya1996s@gmail.com>
2023-02-24 19:28:12 +0900

    02/22

--- toshiya.js
+++ toshiya.js
@@ -1,3 +1,12 @@
 $(function () {
-    
+    $(".btn").on("click", function () {
+        // console.log($(".hundredNum").val());
+        // console.log($(".tenNum").val());
+        // console.log($(".oneNum").val());
+        // console.log($(".hundredNum").val()+$(".tenNum").val()+$(".oneNum").val());
+        let nekoNum = $(".hundredNum").val()+$(".tenNum").val()+$(".oneNum").val()
+         console.log(nekoNum);
+
+        $(".nekoimg").append(`<p>https://http.cat/${nekoNum}</p>`)
+    })
 })
